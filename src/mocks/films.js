50a6e97adfb8f186a1d459de9eeff159e347107b
pipols <pipols2007@gmail.com>
2020-02-17 02:38:33 +0300

    моки для карточек фильмов

---
+++ src/mocks/films.js
@@ -0,0 +1,25 @@
+export default filmsData = [{
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`
+}, {
+  title: `Bohemian Rhapsody`,
+  poster: `bohemian-rhapsody.jpg`
+}, {
+  title: `Macbeth`,
+  poster: `macbeth.jpg`
+}, {
+  title: `Aviator`,
+  poster: `aviator.jpg`
+}, {
+  title: `We need to talk about Kevin`,
+  poster: `we-need-to-talk-about-kevin.jpg`
+}, {
+  title: `What We Do in the Shadows`,
+  poster: `what-we-do-in-the-shadows.jpg`
+}, {
+  title: `Revenant`,
+  poster: `revenant.jpg`
+}, {
+  title: `Johnny English`,
+  poster: `johnny-english.jpg`
+}];
