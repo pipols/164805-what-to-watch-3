a2099d88f324d73cc9e9bc1660ba9fabcea6db90
pipols <pipols2007@gmail.com>
2020-02-24 11:40:43 +0300

    добавил роутинг в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
+import MoviePage from "../movie-page/movie-page.jsx";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const onMovieTitleClick = () => {};
 
-const App = ({promoMovieData, filmsData}) => {
-  return (
-    <Main
-      promoMovieData={promoMovieData}
-      filmsData={filmsData}
-      onMovieTitleClick={onMovieTitleClick}
-    />
-  );
-};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {promoMovieData, filmsData} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main
+              promoMovieData={promoMovieData}
+              filmsData={filmsData}
+              onMovieTitleClick={onMovieTitleClick}
+            />
+          </Route>
+          <Route exact path="/movie-page">
+            <MoviePage/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   promoMovieData: PropTypes.shape({
