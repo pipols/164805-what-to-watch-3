e9bc3697efecc671ec392c0decd08f23d5740483
pipols <pipols2007@gmail.com>
2020-02-25 17:53:48 +0300

    добавил видео превью в моки

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,25 +1,33 @@
 export const filmsData = [{
   title: `Fantastic Beasts: The Crimes of Grindelwald`,
-  poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  poster: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `Bohemian Rhapsody`,
-  poster: `bohemian-rhapsody.jpg`
+  poster: `bohemian-rhapsody.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `Macbeth`,
-  poster: `macbeth.jpg`
+  poster: `macbeth.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `Aviator`,
-  poster: `aviator.jpg`
+  poster: `aviator.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `We need to talk about Kevin`,
-  poster: `we-need-to-talk-about-kevin.jpg`
+  poster: `we-need-to-talk-about-kevin.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `What We Do in the Shadows`,
-  poster: `what-we-do-in-the-shadows.jpg`
+  poster: `what-we-do-in-the-shadows.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `Revenant`,
-  poster: `revenant.jpg`
+  poster: `revenant.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }, {
   title: `Johnny English`,
-  poster: `johnny-english.jpg`
+  poster: `johnny-english.jpg`,
+  preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 }];
