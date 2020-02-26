7b79bb574dbe272407d7a65236b30de0d9293229
pipols <pipols2007@gmail.com>
2020-02-26 17:09:38 +0300

    убрал лишнее в видеоплеере

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -4,19 +4,20 @@ import PropTypes from "prop-types";
 class VideoPlayer extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.videoRef = React.createRef();
-    this.state = {
-      isPlay: props.isPlay
-    };
   }
 
   render() {
     const {preview, poster} = this.props;
 
     return (
-      <video ref={this.videoRef} src={preview} autoPlay poster={poster} muted width="280" height="175"/>
+      <video src={preview} autoPlay poster={poster} muted width="280" height="175"/>
     );
   }
 }
+
+VideoPlayer.propTypes = {
+  preview: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
 
 export default VideoPlayer;
