2386c18f331d10898c83db27b5e5ecf86d7278ba
pipols <pipols2007@gmail.com>
2020-03-02 19:07:31 +0300

    правки

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TabName = {
+  OVERVIEW: `OVERVIEW`,
+  DETAILS: `DETAILS`,
+  REVIEWS: `REVIEWS`
+};
+
+const NAV_ACTIVE_CLASS = `movie-nav__item--active`;
+
+const Tabs = (props) => {
+  const {onTabClick, activeTab} = props;
+
+  const setActiveClass = (currentTab) => {
+    return activeTab === currentTab ? NAV_ACTIVE_CLASS : ``;
+  };
+
+  return (
+    <ul className="movie-nav__list">
+      <li className={`movie-nav__item ${setActiveClass(TabName.OVERVIEW)}`}>
+        <a onClick={() => onTabClick(TabName.OVERVIEW)} href="#" className="movie-nav__link">Overview</a>
+      </li>
+      <li className={`movie-nav__item ${setActiveClass(TabName.DETAILS)}`}>
+        <a onClick={() => onTabClick(TabName.DETAILS)} href="#" className="movie-nav__link">Details</a>
+      </li>
+      <li className={`movie-nav__item ${setActiveClass(TabName.REVIEWS)}`}>
+        <a onClick={() => onTabClick(TabName.REVIEWS)} href="#" className="movie-nav__link">Reviews</a>
+      </li>
+    </ul>
+  );
+};
+
+Tabs.propTypes = {
+  onTabClick: PropTypes.func.isRequired,
+  activeTab: PropTypes.string.isRequired
+};
+
+export default Tabs;
