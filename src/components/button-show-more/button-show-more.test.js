1d55e1c89890d5548c6a77ed8dd6c0fff82ecad2
pipols <pipols2007@gmail.com>
2020-03-15 12:19:56 +0300

    убрал Provider из теста

--- src/components/button-show-more/button-show-more.test.js
+++ src/components/button-show-more/button-show-more.test.js
@@ -1,22 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {Provider} from "react-redux";
-import configureStore from "redux-mock-store";
 import {ButtonShowMore} from "./button-show-more.jsx";
 
-const mockStore = configureStore([]);
 const onShowMoreClick = () => {};
 
 it(`<Film /> renders correctly`, () => {
-  const store = mockStore({
-    shownCardsStack: 8
-  });
 
   const tree = renderer
-  .create(
-      <Provider store={store}>
-        <ButtonShowMore onShowMoreClick={onShowMoreClick} />
-      </Provider>)
+  .create(<ButtonShowMore onShowMoreClick={onShowMoreClick} />)
   .toJSON();
 
   expect(tree).toMatchSnapshot();
