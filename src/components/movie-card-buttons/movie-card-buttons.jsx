b75f4b0b8a3a418ca22a6ed6cd0e8f7db04de0c2
pipols <pipols2007@gmail.com>
2020-03-27 23:37:28 +0300

    вынес кнопки просмотр фильма, добавить отзыв, мой список в компонент

---
+++ src/components/movie-card-buttons/movie-card-buttons.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import {getId} from "../../reducer/state/selector";
+
+const MovieCardButtons = ({id, isMainPage}) => (
+  <div className="movie-card__buttons">
+
+    <Link to={`/player/${id}`} className="btn btn--play movie-card__button" type="button">
+      <svg viewBox="0 0 19 19" width={19} height={19}>
+        <use xlinkHref="#play-s"/>
+      </svg>
+      <span>Play</span>
+    </Link>
+
+    <Link to="/mylist" className="btn btn--list movie-card__button" type="button">
+      <svg viewBox="0 0 19 20" width={19} height={20}>
+        <use xlinkHref="#add"/>
+      </svg>
+      <span>My list</span>
+    </Link>
+
+    {isMainPage || <Link to={`/films/${id}/review`} className="btn movie-card__button">
+      Add review
+    </Link>}
+
+  </div>
+);
+// Player (/player/:id)
+// /mylist
+MovieCardButtons.propTypes = {
+  id: PropTypes.number.isRequired,
+  isMainPage: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  id: getId(state)
+});
+
+export {MovieCardButtons};
+export default connect(mapStateToProps)(MovieCardButtons);
