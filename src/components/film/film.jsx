3835a7c77b1d73a7b3e90b7ce92f12e63f03b689
pipols <pipols2007@gmail.com>
2020-03-29 11:38:52 +0300

    изменил логику рендера Film

--- src/components/film/film.jsx
+++ src/components/film/film.jsx
@@ -10,83 +10,83 @@ import Tabs from "../tabs/tabs.jsx";
 import {TabName} from "../../const/common";
 import {getfilmsByGenre} from "../../utils/utils";
 import {CardCount} from "../../const/common";
-import {ActionCreator} from "../../reducer/state/state";
 import {getFilms} from "../../reducer/data/selector";
 import {getGenre, getFilm} from "../../reducer/state/selector";
 import {DataOperation} from "../../reducer/data/data";
 import withActiveItem from "../../hocs/with-active-item/with-active-item.jsx";
 import Header from "../header/header.jsx";
 import MovieCardButtons from "../movie-card-buttons/movie-card-buttons.jsx";
+import Preloader from "../preloader/preloader.jsx";
 
 class Film extends React.PureComponent {
   constructor(props) {
     super(props);
   }
 
-  componentDidUpdate() {
-    this.props.onFilmIdSet(this.props.match.params.id);
-    this.props.onCommentsMount(this.props.match.params.id);
+  component() {
+    const activeItem = this.props.activeItem || TabName.OVERVIEW;
+    const {poster, cover, title, genre, year} = this.props.film;
+    return (
+      <React.Fragment>
+        <section className="movie-card movie-card--full">
+          <div className="movie-card__hero">
+            <div className="movie-card__bg">
+              <img src={poster} alt={title}/>
+            </div>
+            <h1 className="visually-hidden">WTW</h1>
+
+            <Header />
+
+            <div className="movie-card__wrap">
+              <div className="movie-card__desc">
+                <h2 className="movie-card__title">{title}</h2>
+                <p className="movie-card__meta">
+                  <span className="movie-card__genre">{genre}</span>
+                  <span className="movie-card__year">{year}</span>
+                </p>
+
+                <MovieCardButtons />
+
+              </div>
+            </div>
+          </div>
+          <div className="movie-card__wrap movie-card__translate-top">
+            <div className="movie-card__info">
+              <div className="movie-card__poster movie-card__poster--big">
+                <img src={cover} alt={title + `poster`} width={218} height={327}/>
+              </div>
+              <div className="movie-card__desc">
+                <nav className="movie-nav movie-card__nav">
+                  <Tabs onItemClick={this.props.onItemClick} activeItem={activeItem} />
+                </nav>
+
+                {activeItem === TabName.OVERVIEW && <Overview {...this.props.film} />}
+                {activeItem === TabName.DETAILS && <Details film={this.props.film} />}
+                {activeItem === TabName.REVIEWS && <Reviews />}
+
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="page-content">
+          <section className="catalog catalog--like-this">
+            <h2 className="catalog__title">
+            More like this
+            </h2>
+            <MoviesList films={
+              getfilmsByGenre(this.props.films, this.props.currentGenre)
+            .slice(0, CardCount.SIMILAR)
+            } />
+          </section>
+          <Footer />
+        </div>
+      </React.Fragment>);
   }
 
   render() {
-    this.props.onFilmIdSet(this.props.match.params.id);
-    const {poster, cover, title, genre, year} = this.props.film;
-    const activeItem = this.props.activeItem || TabName.OVERVIEW;
-
-    return (<React.Fragment>
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__hero">
-          <div className="movie-card__bg">
-            <img src={poster} alt={title}/>
-          </div>
-          <h1 className="visually-hidden">WTW</h1>
-
-          <Header />
-
-          <div className="movie-card__wrap">
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">{title}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{genre}</span>
-                <span className="movie-card__year">{year}</span>
-              </p>
-
-              <MovieCardButtons />
-
-            </div>
-          </div>
-        </div>
-        <div className="movie-card__wrap movie-card__translate-top">
-          <div className="movie-card__info">
-            <div className="movie-card__poster movie-card__poster--big">
-              <img src={cover} alt={title + `poster`} width={218} height={327}/>
-            </div>
-            <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <Tabs onItemClick={this.props.onItemClick} activeItem={activeItem} />
-              </nav>
-
-              {activeItem === TabName.OVERVIEW && <Overview {...this.props.film} />}
-              {activeItem === TabName.DETAILS && <Details film={this.props.film} />}
-              {activeItem === TabName.REVIEWS && <Reviews />}
-
-            </div>
-          </div>
-        </div>
-      </section>
-      <div className="page-content">
-        <section className="catalog catalog--like-this">
-          <h2 className="catalog__title">
-            More like this
-          </h2>
-          <MoviesList films={
-            getfilmsByGenre(this.props.films, this.props.currentGenre)
-            .slice(0, CardCount.SIMILAR)
-          } />
-        </section>
-        <Footer />
-      </div>
-    </React.Fragment>);
+    return this.props.film
+      ? this.component()
+      : <Preloader />;
   }
 }
 
@@ -129,19 +129,10 @@ Film.propTypes = {
     videoLink: PropTypes.string.isRequired,
     preview: PropTypes.string.isRequired,
   })),
-  id: PropTypes.number,
   currentGenre: PropTypes.string.isRequired,
-
   onCommentsMount: PropTypes.func.isRequired,
-  onFilmIdSet: PropTypes.func.isRequired,
-
   onItemClick: PropTypes.func.isRequired,
   activeItem: PropTypes.string,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.number
-    })
-  }),
 };
 
 const mapStateToProps = (state) => ({
@@ -154,9 +145,6 @@ const mapDispatchToProps = (dispatch) => ({
   onCommentsMount(id) {
     dispatch(DataOperation.loadComments(id));
   },
-  onFilmIdSet(id) {
-    dispatch(ActionCreator.setId(parseInt(id, 10)));
-  }
 });
 
 export {Film};
